f3fc84dbb1097dba3505c0f7d1d97f1cc6cebb5e
pushpinder001 <pushpinder.1@iitj.ac.in>
2016-09-27 22:53:35 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<set>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<map>
+#define M 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii > vii;
+int main(){
+    return 0;
+}
